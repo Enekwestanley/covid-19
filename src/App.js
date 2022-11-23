a65a5a6c7f98ddb14437074c5750e749df944716
stanley <tochukwuu14@gmail.com>
2022-11-23 19:20:09 +0100

    Add home page to dispaly covid cases and countries

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
+import { useDispatch } from 'react-redux';
+import React, { useEffect } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { fetchCountriesData } from './Redux/covidData/countriesCovidData';
+import HomePage from './components/home/Home';
+import Details from './components/details/Details';
 import './App.css';
 
-function App() {
+const App = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchCountriesData());
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<HomePage />} />
+        <Route exact path="/details/:country" element={<Details />} />
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
